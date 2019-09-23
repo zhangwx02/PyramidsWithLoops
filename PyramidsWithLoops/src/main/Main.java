9609d7039190e4393ec27c099bf477e5f111407f
zhangwx <zhangwx@zhangs-mbp.wifi.uc.edu>
2019-09-23 16:43:13 -0400

    The equicrural triangle

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -15,12 +15,14 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 4; j >= i; j--) {
+				System.out.print(" ");
 			}
-			System.out.println("");
+			for (int k = 1; k <= i * 2 -1; k++) {
+			System.out.print(i);
 		}
+				System.out.println();
+	}
 	}
 }
